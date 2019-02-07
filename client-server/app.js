ad4cd000b5b82ad1800d6480f20ce63e28c36e39
Denis Alayza <denis.alayza@gmail.com>
2019-02-07 00:42:07 -0200

    Limit bytes send in POST

--- client-server/app.js
+++ client-server/app.js
@@ -14,8 +14,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' })); // tamanho maximo que recibo de la imagen
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
